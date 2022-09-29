9aa263a0555d802da3bccb3fbf060cdd53abaa4d
renetaBoneva <reneta.boneva@abv.bg>
2022-09-29 13:01:03 +0300

    Create addAndSubtract.js

---
+++ Fundamentals JS/Functions - Exercise/addAndSubtract.js
@@ -0,0 +1,9 @@
+function addAndSubtract(n1, n2, n3){
+    let sum = (n1, n2) => n1 + n2;
+    let subtract = sum(n1, n2) - n3;
+    return subtract;
+}
+console.log(addAndSubtract(23,
+    6,
+    10
+    ))
