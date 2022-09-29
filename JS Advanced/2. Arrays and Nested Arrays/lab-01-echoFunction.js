e2087c8d9a458a562c3e955eabb7fc1614e36ea1
renetaBoneva <reneta.boneva@abv.bg>
2022-09-29 12:29:27 +0300

    Delete JS Advanced/2. Arrays and Nested Arrays directory

--- JS Advanced/2. Arrays and Nested Arrays/lab-01-echoFunction.js
+++
@@ -1,8 +0,0 @@
-function echoFunction(input){
-    
-    let inputLength = input.length;
-    console.log(inputLength);
-    console.log(input);
-}
-echoFunction('Hello, JavaScript!');
-echoFunction('strings are easy')
--- JS Advanced/2. Arrays and Nested Arrays/lab-03-largestNum.js
+++
@@ -1,6 +0,0 @@
-function largestNum(firstNum, secondNum, thirdNum) {
-    let largestNum = Math.max(firstNum, secondNum, thirdNum);
-    console.log(`The largest number is ${largestNum}.`);
-}
-largestNum(5, -3, 16);
-largestNum(-3, -5, -22.5)
