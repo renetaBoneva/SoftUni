77e0281fc97c794674feae293388c490e5f3789d
renetaBoneva <reneta.boneva@abv.bg>
2022-09-29 12:30:08 +0300

    Create ex-01-givenDelimer.js

---
+++ JS Advanced/2. Arrays and Nested Arrays/ex-01-givenDelimer.js
@@ -0,0 +1,19 @@
+function givnDelimiter(array, delimiter){
+    console.log(array.join(delimiter));
+
+}
+givnDelimiter(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+);
+givnDelimiter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+);
