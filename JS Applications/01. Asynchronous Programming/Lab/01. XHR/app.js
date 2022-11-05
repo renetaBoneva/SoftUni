0b7e1953ef13ec1488ced2bd0beee6f5b971aba8
renetaBoneva <reneta.boneva@abv.bg>
2022-11-05 21:50:57 +0200

    New exercise

---
+++ JS Applications/01. Asynchronous Programming/Lab/01. XHR/app.js
@@ -0,0 +1,11 @@
+function loadRepos() {
+   let url = 'https://api.github.com/users/testnakov/repos';
+   const httpRequest = new XMLHttpRequest();
+   httpRequest.addEventListener('readystatechange', ()=>{
+      if(httpRequest.readyState == 4 && httpRequest.status == 200){
+         document.querySelector('#res').textContent = httpRequest.responseText;
+      }
+   })
+   httpRequest.open('GET', url);
+   httpRequest.send();
+}
