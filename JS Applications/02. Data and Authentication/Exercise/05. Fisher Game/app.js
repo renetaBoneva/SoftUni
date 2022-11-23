58ef8d27681078c492cbc9ac11522a6874e5d4f9
renetaBoneva <reneta.boneva@abv.bg>
2022-11-23 17:52:00 +0200

    Update app.js

--- JS Applications/02. Data and Authentication/Exercise/05. Fisher Game/app.js
+++ JS Applications/02. Data and Authentication/Exercise/05. Fisher Game/app.js
@@ -16,16 +16,15 @@ if (!sessionStorage.accessToken) {
 }
 
 let fieldset = document.querySelector('fieldset');
-let p = document.createElement('p');
-p.textContent = 'Click to load catches'
-document.querySelector('fieldset #catches').remove();
-fieldset.appendChild(p)
+fieldset.innerHTML = 'Click to load catches'
 
-async function logout(){
-    let response = await fetch('http://localhost:3030/users/logout',{
+async function logout() {
+    let response = await fetch('http://localhost:3030/users/logout', {
         method: "DELETE",
-        headers:{'X-Authorization': sessionStorage.accessToken,
-                'Content-Type': 'application/json'}
+        headers: {
+            'X-Authorization': sessionStorage.accessToken,
+            'Content-Type': 'application/json'
+        }
     })
 
     sessionStorage.clear()
@@ -37,14 +36,14 @@ let loadBtn = document.querySelector('#home-view aside button.load');
 loadBtn.disabled = false
 loadBtn.addEventListener('click', getCatchesInfo);
 
-async function getCatchesInfo(){
+async function getCatchesInfo() {
     let response = await fetch('http://localhost:3030/data/catches');
     let data = await response.json()
     displayCatches(data)
 }
 
-function displayCatches(allCatchesArr){
-    fieldset.querySelector('p').remove();
+function displayCatches(allCatchesArr) {
+    fieldset.innerHTML = '';
     let catchesDIV = document.createElement('div')
     catchesDIV.setAttribute('id', 'catches')
 
@@ -71,13 +70,13 @@ function displayCatches(allCatchesArr){
         catchesDIV.appendChild(currentCatchDIV)
     })
 
-    
+
     fieldset.appendChild(catchesDIV)
 
     let updateBtnsArr = document.getElementsByClassName('update');
     Array.from(updateBtnsArr).forEach(btn => {
         btn.addEventListener('click', updateCatch)
-        if(btn.getAttribute('owner-id') != sessionStorage._id){
+        if (btn.getAttribute('owner-id') != sessionStorage._id) {
             btn.disabled = true;
         } else {
             btn.disabled = false;
@@ -87,41 +86,105 @@ function displayCatches(allCatchesArr){
     let deleteBtnsArr = document.getElementsByClassName('delete');
     Array.from(deleteBtnsArr).forEach(btn => {
         btn.addEventListener('click', deleteCatch)
-        if(btn.getAttribute('owner-id') != sessionStorage._id){
+        if (btn.getAttribute('owner-id') != sessionStorage._id) {
             btn.disabled = true;
         } else {
             btn.disabled = false;
-        }})
+        }
+    })
 }
 
-function updateCatch(event){
-    let btn = event.target;
-    let angler = btn.parentNode.getElementsByClassName('angler')[0].value;
-    let weight = btn.parentNode.getElementsByClassName('weight')[0].value ;
-    let species = btn.parentNode.getElementsByClassName('species')[0].value ;
-    let location = btn.parentNode.getElementsByClassName('location')[0].value ;
-    let bait = btn.parentNode.getElementsByClassName('bait')[0].value ;
-    let captureTime = btn.parentNode.getElementsByClassName('captureTime')[0].value ;
-    let body = {angler, weight, species, location, bait};;
+function updateCatch(event) {
+    try {
+        let btn = event.target;
+        let angler = btn.parentNode.getElementsByClassName('angler')[0].value;
+        let weight = btn.parentNode.getElementsByClassName('weight')[0].value;
+        let species = btn.parentNode.getElementsByClassName('species')[0].value;
+        let location = btn.parentNode.getElementsByClassName('location')[0].value;
+        let bait = btn.parentNode.getElementsByClassName('bait')[0].value;
+        let captureTime = btn.parentNode.getElementsByClassName('captureTime')[0].value;
 
-    let dataId = btn.getAttribute('data-id');
-    putRequestUpdatedCatch(dataId, body)
+        if (angler == "" || weight == "" || species == ""
+            || location == "" || bait == "" || captureTime == "") {
+            throw new Error('Моля, попълнете всички полета!')
+        }
+        let body = { angler, weight, species, location, bait, captureTime };
+
+        let dataId = btn.getAttribute('data-id');
+        putRequestUpdatedCatch(dataId, body)
+    }
+    catch (error) {
+        alert(error.message)
+    }
 }
-async function putRequestUpdatedCatch(dataId, body){
+async function putRequestUpdatedCatch(dataId, body) {
     let response = await fetch(`http://localhost:3030/data/catches/${dataId}`, {
         method: 'PUT',
-        headers: {'X-Authorization': sessionStorage.accessToken,
-                    'Content-Type': "application/json"},
+        headers: {
+            'X-Authorization': sessionStorage.accessToken,
+            'Content-Type': "application/json"
+        },
         body: JSON.stringify(body)
     })
 }
 
-async function deleteCatch(event){    
+async function deleteCatch(event) {
     let btn = event.target;
     let dataId = btn.getAttribute('data-id');
-    
+
     let response = await fetch(`http://localhost:3030/data/catches/${dataId}`, {
         method: "Delete",
-        headers: {'Content-type': 'application/json'}
+        headers: {
+            'X-Authorization': sessionStorage.accessToken,
+            'Content-type': 'application/json'
+        }
+    })
+    getCatchesInfo()
+}
+
+let addForm = document.getElementById('addForm');
+addForm.addEventListener('submit', addCatch)
+
+
+function addCatch(event) {
+    event.preventDefault()
+    try {
+        let form = new FormData(addForm);
+        let angler = form.get('angler')
+        form.set('angler', "")
+        let weight = form.get('weight');
+        let species = form.get('species');
+        let location = form.get('location');
+        let bait = form.get('bait');
+        let captureTime = form.get('captureTime');
+        if (angler == "" || weight == "" || species == ""
+            || location == "" || bait == "" || captureTime == "") {
+            throw new Error('Моля, попълнете всички полета!')
+        }
+        let body = { angler, weight, species, location, bait, captureTime };
+        // Array.from(form.entries())
+        //             .forEach(([name, v]) => {
+        //                 console.log(form.get(name))
+        //                 form.set(name, '')
+        //                 console.log(form.get(name))
+        //             })
+        postNewCatch(body)
+        getCatchesInfo()
+    }
+    catch (error) {
+        alert(error.message)
+    }
+
+}
+
+async function postNewCatch(body) {
+    let token = sessionStorage.accessToken;
+    let response = await fetch('http://localhost:3030/data/catches', {
+        method: 'POST',
+        headers: {
+            'X-Authorization': token,
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(body)
     })
 }
