18d0c963b6cbb58dacc0b34e601708546e654568
renetaBoneva <reneta.boneva@abv.bg>
2022-11-23 17:55:01 +0200

    Update register.js

--- JS Applications/02. Data and Authentication/Exercise/05. Fisher Game/register.js
+++ JS Applications/02. Data and Authentication/Exercise/05. Fisher Game/register.js
@@ -8,39 +8,39 @@ if (!sessionStorage.accessToken) {
 let registerForm = document.querySelector('form');
 registerForm.addEventListener('submit', registerUser)
 
-function registerUser(e){
+async function registerUser(e) {
     e.preventDefault();
-    try{
+    try {
         let form = new FormData(registerForm);
         let email = form.get('email');
         let password = form.get('password');
         let rePass = form.get('rePass');
 
-        if(!email.includes('@') || email == ""){
-            throw new Error('Невалиден имейл!'); 
+        if (!email.includes('@') || email == "") {
+            throw new Error('Невалиден имейл!');
         }
 
-        if(password !== rePass || password == "" || rePass == ""){
-            throw new Error('Невалидна парола!'); 
+        if (password !== rePass || password == "" || rePass == "") {
+            throw new Error('Невалидна парола!');
         }
-        postNewUserDataToTheServer({email, password})
+
+        let response = await fetch('http://localhost:3030/users/register', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
+        let data = await response.json();
+
+        if (response.ok != true) {
+            throw new Error(data.message);
+        }
+        sessionStorage.setItem('accessToken', data.accessToken)
+        sessionStorage.setItem('email', data.email)
+        sessionStorage.setItem('_id', data._id)
+        window.location = './index.html';
+
     }
-    catch(error){
+    catch (error) {
         alert(error.message)
     }
 }
-
-async function postNewUserDataToTheServer(body){
-    let response = await fetch('http://localhost:3030/users/register', {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(body)
-    })
-
-    let token = await response.json();
-    sessionStorage.setItem('accessToken', token.accessToken)
-    sessionStorage.setItem('email', token.email)
-    sessionStorage.setItem('_id', token._id)
-    window.location = './index.html';
-}
-// debugger
