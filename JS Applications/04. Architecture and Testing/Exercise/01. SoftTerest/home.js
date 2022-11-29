d3fe3b17ccbcfe8a7b4c57d85a350931c61afc83
renetaBoneva <reneta.boneva@abv.bg>
2022-11-29 20:23:50 +0200

    Update home.js

--- JS Applications/04. Architecture and Testing/Exercise/01. SoftTerest/home.js
+++ JS Applications/04. Architecture and Testing/Exercise/01. SoftTerest/home.js
@@ -1,8 +1,19 @@
+import { onLogout } from "../api/user.js";
 
 let section = document.getElementById('home-view')
 
+let context;
 export function showHome(ctx){
     let getStartedBtn = section.querySelector('a');
     getStartedBtn.addEventListener('click', ctx.onNav);
     ctx.showSection(section)
+
+    context = ctx;
+}
+
+export async function handleLogout(event){
+    event.preventDefault();
+
+    await onLogout()
+    context.goTo('/home-view')
 }
