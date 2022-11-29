9fccb83aca2d582ddb2020a9a86dd8a9b66204ce
renetaBoneva <reneta.boneva@abv.bg>
2022-11-29 20:17:27 +0200

    Update details.js

--- JS Applications/04. Architecture and Testing/Exercise/01. SoftTerest/details.js
+++ JS Applications/04. Architecture and Testing/Exercise/01. SoftTerest/details.js
@@ -1,26 +1,33 @@
+import * as api from "../api/api.js";
+
 let section = document.getElementById('details-view')
-
-export function crateDetailsView(detailsData) {
-    section.innerHTML = `<img class="det-img" src="${detailsData.img}" />
-    <div class="desc">
-        <h2 class="display-5">${detailsData.title}</h2>
-        <p class="infoType">Description:</p>
-        <p class="idea-description">${detailsData.description}</p>
-    </div>`;
-
-    console.log('TO CHECH IF THE USER IS CREATOR');
-    if (true) {
-        section.innerHTML += `
-        <div class="text-center" _ownerId="${detailsData._ownerId}">
-            <a class="btn detb" href="/delete">Delete</a>
-        </div>`
-    }
-    section.querySelector('a').addEventListener('click', (event)=>{
-        event.preventDefault()
-        console.log('DELETE HANDLER');
-    })
-}
+let context;
 
 export function showDetails(ctx) {
     ctx.showSection(section)
+    context = ctx;
+}
+
+export function crateDetailsView(detailsData) {
+    section.innerHTML = `<img class="det-img" src="${detailsData.img}" />
+        <div class="desc">
+            <h2 class="display-5">${detailsData.title}</h2>
+            <p class="infoType">Description:</p>
+            <p class="idea-description">${detailsData.description}</p>
+        </div>`;
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+        if (user._id == detailsData._ownerId) {
+            section.innerHTML += `
+                <div class="text-center" _ownerId="${detailsData._ownerId}">
+                    <a class="btn detb" href="/delete">Delete</a>
+                </div>`
+            section.querySelector('a').addEventListener('click', async (event) => {
+                event.preventDefault()
+                await api.del(`data/ideas/${detailsData._id}`);
+                context.goTo('/dashboard-holder')
+            })
+        }
+    }
 }
