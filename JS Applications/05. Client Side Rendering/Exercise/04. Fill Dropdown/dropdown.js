e9440a7aaad265cc48191aa01296f3bf4329d2a5
renetaBoneva <reneta.boneva@abv.bg>
2022-12-01 11:43:31 +0200

    Create dropdown.js

---
+++ JS Applications/05. Client Side Rendering/Exercise/04. Fill Dropdown/dropdown.js
@@ -0,0 +1,39 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+const root = document.getElementById('menu');
+
+const newItemForm = document.querySelector('form');
+newItemForm.addEventListener('submit', addItem)
+
+async function addItem(event) {
+    event.preventDefault();
+    let itemText = document.getElementById('itemText').value;
+    if(itemText == ""){
+        return alert('Please, enter item text!')
+    }
+    await postNewItem({'text': itemText})
+    loadOptions()
+    newItemForm.reset()
+}
+
+loadOptions()
+
+async function loadOptions() {
+    let response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown')
+    let options = await response.json();
+
+    let toLoad = Array.from(Object.values(options)).map(option => {
+        let optionNode = html`<option value="${option._id}">${option.text}</option>`
+        return optionNode
+    })
+    
+    render(toLoad, root)
+}
+
+async function postNewItem(body){
+    let response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown', {
+        method: 'post',
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(body)
+    })
+}
