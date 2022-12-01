1f0ff5f97f09de5a742942a3bcae25babd92ce76
renetaBoneva <renkata02@abv.bg>
2022-12-01 16:02:46 +0200

    Add files via upload

---
+++ JS Applications/05. Client Side Rendering/Exercise/06. Book Library/api.js
@@ -0,0 +1,49 @@
+let host = 'http://localhost:3030/'
+
+
+async function requester(method, url, body) {
+
+    let options = {
+        method,
+        headers: {}
+    };
+
+    if (body) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(body);
+    }
+
+    let user = localStorage.getItem('user');
+    if (user) {
+        options.headers['X-Authorization'] = JSON.parse(user).accessToken;
+    }
+
+    try {
+        let response = await fetch(host + url, options)
+        if (!response.ok) {
+            if(response.status == 403){
+                localStorage.removeItem('user')
+            }
+            let err = await response.json();
+            throw new Error(err.message);
+        }
+        if (response.status != 204) {
+            return await response.json();
+        }
+    } catch (error) {
+        alert(error.message);
+        throw error;
+    }
+}
+
+let get = requester.bind(null, 'get')
+let post = requester.bind(null, 'post')
+let put = requester.bind(null, 'put')
+let del = requester.bind(null, 'delete')
+
+export {
+    get,
+    post, 
+    put, 
+    del
+}
