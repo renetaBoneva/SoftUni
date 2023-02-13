d99db9bba12749ac2153d7cdd8d7b0bc508fc96e
Ренета Бонева <reneta.boneva@abv.bg>
2023-02-13 10:05:24 +0200

    Update cubeController.js

--- JS Back-End/Workshop: ExpressJS and Templating/src/controllers/cubeController.js
+++ JS Back-End/Workshop: ExpressJS and Templating/src/controllers/cubeController.js
@@ -1,3 +1,23 @@
-exports.getCubeController = (req, res)=>{
+const Cube = require('../models/Cube');
+const db = require('../db.json')
+
+exports.getCreateCube = (req, res)=>{
     res.render('create');
 };
+
+exports.postCreateCube = (req, res)=>{
+    const {name, description, imageUrl, difficultyLevel} = req.body;
+    const newCube = new Cube(name, description, imageUrl, difficultyLevel);
+    newCube.save();
+
+    res.redirect('/');
+}
+
+exports.getCubeDetails = (req, res) => {
+    const cubeId = req.params.cubeId;
+    const ourCube = db.cubes.find(cube => cube.id == cubeId );
+    if(!ourCube){
+        res.redirect('/404')
+    }
+    res.render('details', ourCube)
+}
