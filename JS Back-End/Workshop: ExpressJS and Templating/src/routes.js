2f3d82a8fb7b1e5682f84ba1728af1c6fa1fc2a3
Ренета Бонева <reneta.boneva@abv.bg>
2023-02-13 10:10:25 +0200

    Update routes.js

--- JS Back-End/Workshop: ExpressJS and Templating/src/routes.js
+++ JS Back-End/Workshop: ExpressJS and Templating/src/routes.js
@@ -7,7 +7,9 @@ const homeController = require('./controllers/homeController')
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 
-router.get('/create', cubeController.getCubeController);
+router.get('/create', cubeController.getCreateCube);
+router.post('/create', cubeController.postCreateCube);
+router.get('/details/:cubeId', cubeController.getCubeDetails)
 
 router.get('*', (req, res)=>{
     res.render('404');
