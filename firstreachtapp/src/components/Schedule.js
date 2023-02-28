554864ecd9f2572d71bbdd577e12fbc1dd9e78a6
Ренета Бонева <renkata02@abv.bg>
2023-02-28 10:45:13 +0200

    Add files via upload

---
+++ firstreachtapp/src/components/Schedule.js
@@ -0,0 +1,19 @@
+import NavTabs from "./partials/NavTabs";
+import TabPanes from "./partials/TabPanes";
+
+function Schedule() {
+    return (
+        <div className="container">
+            <div className="row me-row schedule" id="schedule">
+                <h2 className="row-title content-ct">Event Schedule</h2>
+                <div className="col-md-12">
+                    <NavTabs />
+
+                    <TabPanes />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Schedule;
