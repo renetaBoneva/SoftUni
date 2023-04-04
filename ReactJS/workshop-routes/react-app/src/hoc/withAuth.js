b9ce2da0c62859b34e6c7152d96a9aa346194db6
Ренета Бонева <reneta.boneva@abv.bg>
2023-04-04 11:05:48 +0300

    Delete ReactJS/workshop-routes directory

--- ReactJS/workshop-routes/react-app/src/hoc/withAuth.js
+++
@@ -1,8 +0,0 @@
-
-export function withAuth(Component){
-    // const authContext = useAuthContext();
-    function WrapperHOC(props) {
-        return <Component {...props} /*auth={authContext}*/ />
-    }
-    return WrapperHOC
-}
--- ReactJS/workshop-routes/react-app/src/hooks/useAuthContext.js
+++
@@ -1,7 +0,0 @@
-import { AuthContext } from "../contexts/AuthContext";
-import { useService } from "./useService";
-
-export function useAuthContext(){
-    const context = useService(AuthContext);
-    return context;
-}
