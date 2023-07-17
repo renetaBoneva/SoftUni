0a49bac6c10e8eef8d15b18a69ef4a20ba20fe16
Ренета Бонева <reneta.boneva@abv.bg>
2023-07-17 23:45:05 +0300

    Delete Angular/Directives-and-Forms-Lab/directives-and-forms-lab directory

--- Angular/Directives-and-Forms-Lab/directives-and-forms-lab/src/app/home/my-structural-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyStructuralDirectiveDirective } from './my-structural-directive.directive';
-
-describe('MyStrutualDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyStructuralDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Angular/Directives-and-Forms-Lab/directives-and-forms-lab/src/app/my-router-link.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyRouterLinkDirective } from './my-router-link.directive';
-
-describe('MyRouterLinkDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyRouterLinkDirective();
-    expect(directive).toBeTruthy();
-  });
-});
