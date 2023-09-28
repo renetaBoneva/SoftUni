199921836363ed70533c1cd618268cc7e9e71c69
Ренета Бонева <reneta.boneva@abv.bg>
2023-09-28 14:07:59 +0300

    Update

--- JS Back-End/11. Retake exam/Gaming team/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-exports.getHomePage = (req, res) =>{
-    res.render('home');
-}
-
-exports.getErrorPage = (req, res) =>{
-    res.render('home/404');
-}
--- JS Back-End/11. Retake exam/Gaming team/services/gameService.js
+++
@@ -1,6 +0,0 @@
-const Game = require('../models/Game')
-
-exports.getAll = () => Game.find({});
-
-exports.create = (name, image, price, genre, description, platform, owner) =>
-            Game.create({name, image, price, genre, description, platform, owner})
