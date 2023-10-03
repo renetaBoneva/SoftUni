312e28883d2ee0c834e4299e832e35333d25d093
Ренета Бонева <renkata02@abv.bg>
2023-10-03 23:29:46 +0300

    Add files via upload

---
+++ JS Back-End/11. Retake exam/Auction House/services/authService.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+const jwt = require('../lib/jsonwebtoken');
+const { secret } = require('../constants');
+
+exports.findByEmail = (email) => User.findOne({ email })
+
+exports.register = async (firstName, lastName, email, password, rePass) => {
+    console.log({firstName, lastName, email, password, rePass});
+
+    if(password.trim().length < 5) {
+        throw new Error('Password should be at least 5 characters!!');
+    }
+    
+    if (password !== rePass) {
+        throw new Error('Password mismatch!');
+    }
+
+    const existingUser = await this.findByEmail(email);
+    if (existingUser) {
+        throw new Error('User exists!');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        await User.create({ firstName, lastName, email, password: hashedPassword });
+        return this.login(email, password);
+    } catch (err) {
+        return err;
+    }
+}
+
+exports.login = async (email, password) => {
+    const user = await this.findByEmail(email);
+
+    if (!user) {
+        throw new Error('Invalid username or password!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid username or password!');
+    }
+
+    const payload = {
+        _id: user._id,
+        email,
+        username: user.username
+    }
+
+    const token = await jwt.sign(payload, secret);
+    return token;
+}
